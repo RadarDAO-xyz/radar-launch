9f25137952c5365038fe026b488900e7a2f933b2
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-06-20 11:46:13 +0300

    add linting and git hooks

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+/* eslint-env node */
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': ['warn']
+    },
+    root: true
+};
