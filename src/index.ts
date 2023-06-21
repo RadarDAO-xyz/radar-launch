2b1b6545f4380c2e20cb8a3e8f63fbb014eee7b4
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-06-21 17:24:40 +0300

    activate mongodb conn

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './globals';
 import { config } from 'dotenv';
 config();
 import express, { json } from 'express';
-// import { connect } from 'mongoose';
+import { connect } from 'mongoose';
 import { AddressInfo } from 'net';
 import ProjectsRouter from './paths/projects';
 import UsersRouter from './paths/users';
@@ -14,7 +14,7 @@ app.use(json());
 app.use('/projects', ProjectsRouter);
 app.use('/users', UsersRouter);
 
-// connect(process.env.MONGO_URL);
+connect(process.env.MONGO_URL).then(() => console.log('MongoDB connected'));
 
 const server = app.listen(process.env.PORT, () => {
     console.log(
