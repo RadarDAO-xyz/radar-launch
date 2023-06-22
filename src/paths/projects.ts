6166d8d95709f49a4139d02167b778c4014fe4cf
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-06-22 16:27:36 +0300

    add project updates

--- src/paths/projects.ts
+++ src/paths/projects.ts
@@ -1,52 +1,25 @@
-import { Router, Request, NextFunction, Response } from 'express';
+import { Router } from 'express';
 import Project from '../models/Project';
 import { authenticate } from '../util/auth';
 import { create, del, prefetch, read, readMany, update } from '../util/crud';
 import { Types } from 'mongoose';
+import ProjectsUpdatesRouter from './projects/updates';
 
 const ProjectsRouter = Router();
 
 ProjectsRouter.get('/', readMany(Project));
 ProjectsRouter.get('/:id', read(Project));
 
-interface ProjectsPostRequest extends Request {
-    body: Partial<{
-        title: string;
-        description: string;
-        video_url: string;
-        tldr: string;
-        brief:
-            | 'The Enchantress'
-            | 'The Healer'
-            | 'The Mediator'
-            | 'The Teacher'
-            | 'The Artist'
-            | 'Other';
-        inspiration: string;
-        team: { name: string; bio?: string; email: string }[];
-        collaborators?: string;
-        waitlist: boolean;
-        milestones: string[];
-        edition_price: number;
-        mint_end_date: string;
-        benefits: string[];
-        admin_address: string;
-    }>;
-}
-
 ProjectsRouter.use(authenticate(true));
 
-ProjectsRouter.post(
-    '/',
-    async (req: ProjectsPostRequest, res: Response, next: NextFunction) => {
-        (req.body as Record<string, Types.ObjectId>).founder = req.user
-            ?._id as Types.ObjectId;
-        next();
-    }
-);
+ProjectsRouter.post('/', async (req, res, next) => {
+    (req.body as Record<string, Types.ObjectId>).founder = req.user
+        ?._id as Types.ObjectId;
+    next();
+});
 ProjectsRouter.post('/', create(Project));
 
-ProjectsRouter.use(prefetch(Project));
+ProjectsRouter.use('/:id', prefetch(Project));
 
 ProjectsRouter.patch(
     '/:id',
@@ -64,5 +37,7 @@ ProjectsRouter.delete(
         (req) => req.doc?.founder.toString() === req.user?._id.toString()
     )
 );
+
+ProjectsRouter.use('/:projectId/updates', ProjectsUpdatesRouter);
 
 export default ProjectsRouter;
---
+++ src/paths/projects/updates.ts
@@ -0,0 +1,46 @@
+import { Router, urlencoded } from 'express';
+import { create, del, read, readMany, update } from '../../util/crud';
+import ProjectUpdate from '../../models/ProjectUpdate';
+
+const ProjectsUpdatesRouter = Router();
+
+ProjectsUpdatesRouter.get(
+    '/',
+    readMany(ProjectUpdate, () => true, {
+        filter: (req) => ({ project: req.doc?._id })
+    })
+);
+ProjectsUpdatesRouter.get('/:id', read(ProjectUpdate));
+
+ProjectsUpdatesRouter.use(urlencoded({ extended: true }));
+
+ProjectsUpdatesRouter.post('/', (req, res, next) => {
+    req.body.project = req.doc?._id;
+    next();
+});
+ProjectsUpdatesRouter.post(
+    '/',
+    create(
+        ProjectUpdate,
+        (req) => req.doc?.founder.toString() === req.user?._id.toString()
+    )
+);
+
+ProjectsUpdatesRouter.patch(
+    '/:id',
+    update(
+        ProjectUpdate,
+        (req) => req.doc?.founder.toString() === req.user?._id.toString(),
+        { allowedFields: ['text'] }
+    )
+);
+
+ProjectsUpdatesRouter.delete(
+    '/:id',
+    del(
+        ProjectUpdate,
+        (req) => req.doc?.founder.toString() === req.user?._id.toString()
+    )
+);
+
+export default ProjectsUpdatesRouter;
