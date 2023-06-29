9102356e4d81dca027c3702ed47afd50d0d59c5d
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-06-29 23:36:54 +0300

    add https and cors

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,25 @@ import { connect } from 'mongoose';
 import { AddressInfo } from 'net';
 import cookieParser from 'cookie-parser';
 import Routes from './routes';
+import https from 'https';
+import http from 'http';
+import { readFileSync } from 'fs';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(
+        'Received request from',
+        req.headers['user-agent'],
+        'to',
+        req.originalUrl
+    );
+    next();
+});
+
+app.use(cors());
 app.use(json());
 app.use(cookieParser());
 
@@ -16,9 +32,24 @@ app.use(process.env.BASE_URL ?? '/', Routes);
 
 connect(process.env.MONGO_URL).then(() => console.log('MongoDB connected'));
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(
-        'Server listening on port',
-        (server.address() as AddressInfo).port
-    );
-});
+http.createServer((req, res) => {
+    res.writeHead(302, 'Found', {
+        Location: `https://${req.headers.host}${req.url}`
+    }).end();
+}).listen(80);
+
+const server = https
+    .createServer(
+        {
+            cert: readFileSync(path.join(__dirname, '../certificate.crt')),
+            key: readFileSync(path.join(__dirname, '../private.key')),
+            ca: readFileSync(path.join(__dirname, '../ca_bundle.crt'))
+        },
+        app
+    )
+    .listen(443, () => {
+        console.log(
+            'Server listening on port',
+            (server.address() as AddressInfo).port
+        );
+    });
