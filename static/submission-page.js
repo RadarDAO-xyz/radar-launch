3fcf79cf7c304106e313609f87089f0bef32f8ca
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-07-01 01:13:04 +0300

    update submission page (functional?)

--- static/submission-page.js
+++ static/submission-page.js
@@ -1,4 +1,7 @@
 (async function () {
+    const getTextAreaInput = (id) =>
+        tinymce.get(id)?.getContent() || $(`#${id}`).val();
+
     const ids = [
         // String values
         'title',
@@ -13,48 +16,69 @@
 
     const data = {};
 
-    ids.forEach((id) => (data[id] = $(`#${id}`).val()));
+    let submitted = false;
+    $('#submission-form-submit').on('click', async () => {
+        try {
+            if (submitted) return;
+            submitted = true;
 
-    let team = [];
-    for (let i = 1; i <= 8; i++) {
-        team.push({
-            name: $(`#team-name-${i}`).val(),
-            bio: $(`#team-bio-${i}`).val(),
-            email: $(`#team-email-${i}`).val()
-        });
-    }
-    data.team = team.filter((x) => !!x.name);
-    data.waitlist = !!$('#waitlist').val();
+            console.log('submitting');
 
-    let milestones = [];
-    for (let i = 1; i <= 7; i++) {
-        milestones.push({
-            amount: parseInt($(`#milestone-amount-${i}`).val()),
-            text: $(`#milestone-text-${i}`).val()
-        });
-    }
-    data.milestones = milestones.filter((x) => !!x.amount);
+            ids.forEach((id) => (data[id] = getTextAreaInput(id)));
 
-    data.edition_price = parseInt($('#edition_price').val());
+            let team = [];
+            for (let i = 1; i <= 8; i++) {
+                team.push({
+                    name: $(`#team-name-${i}`).val(),
+                    bio: getTextAreaInput(`team-bio-${i}`),
+                    email: $(`#team-email-${i}`).val()
+                });
+            }
+            data.team = team.filter((x) => !!x.name);
+            data.waitlist = $('#waitlist').is(':checked');
 
-    data.mind_end_date = new Date($('#mind_end_date').val()).toISOString();
+            let milestones = [];
+            for (let i = 1; i <= 7; i++) {
+                milestones.push({
+                    amount: parseInt($(`#milestone-amount-${i}`).val()),
+                    text: getTextAreaInput(`milestone-text-${i}`)
+                });
+            }
+            data.milestones = milestones.filter((x) => !!x.amount);
 
-    let benefits = [];
-    for (let i = 1; i <= 7; i++) {
-        benefits.push({
-            amount: parseInt($(`#milestone-amount-${i}`).val()),
-            text: $(`#milestone-text-${i}`).val()
-        });
-    }
-    data.benefits = benefits.filter((x) => !!x.amount);
+            data.edition_price = parseInt($('#edition_price').val());
 
-    const headers = new Headers();
-    headers.set('Content-Type', 'application/json');
-    const res = await fetch(`${API}/projects`, {
-        method: 'POST',
-        headers,
-        body: data
-    }).then((r) => r.json());
+            data.mind_end_date = new Date(
+                $('#mind_end_date').val().replace(/-/g, '/')
+            ).toISOString();
 
-    console.log(res);
+            let benefits = [];
+            for (let i = 1; i <= 7; i++) {
+                benefits.push({
+                    amount: parseInt($(`#benefit-amount-${i}`).val()),
+                    text: getTextAreaInput(`benefit-text-${i}`)
+                });
+            }
+            data.benefits = benefits.filter((x) => !!x.amount);
+
+            const headers = new Headers();
+            headers.set('Content-Type', 'application/json');
+            const res = await fetch(`${API}/projects`, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(data),
+                credentials: 'include'
+            })
+                .then((r) => {
+                    if (!r.ok) throw r.statusText;
+                    else return r;
+                })
+                .then((r) => r.json());
+
+            console.log(res);
+        } catch (e) {
+            submitted = false;
+            console.error(e);
+        }
+    });
 })();
