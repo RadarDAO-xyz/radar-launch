ee910815639e5429e5a84081c9e4f768c098caa7
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-07-01 01:11:45 +0300

    add tinymce to globals

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
     ignorePatterns: ['js/**/*', '*.env'],
     globals: {
         API: 'readonly',
-        $: 'readonly'
+        $: 'readonly',
+        tinymce: 'readonly'
     }
 };
