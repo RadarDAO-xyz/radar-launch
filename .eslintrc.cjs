9d4522ae4c45014013a228b49285757af8e6030b
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-07-03 01:06:30 +0300

    add extractYoutubeId to global vars

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
     globals: {
         API: 'readonly',
         $: 'readonly',
-        tinymce: 'readonly'
+        tinymce: 'readonly',
+        extractYoutubeId: 'readonly'
     }
 };
