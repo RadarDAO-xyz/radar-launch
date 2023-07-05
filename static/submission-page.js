156fd2de7c4d2946b9693b83e24edd349dbd22bb
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-07-06 00:51:06 +0300

    fix form submission

--- static/submission-page.js
+++ static/submission-page.js
@@ -14,6 +14,7 @@
 })();
 
 (async function () {
+    $('#submission-form').on('submit', (e) => e.preventDefault());
     const getTextAreaInput = (id) =>
         tinymce.get(id)?.getContent() || $(`#${id}`).val();
 
