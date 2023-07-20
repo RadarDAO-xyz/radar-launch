fe37f54def2f9ab52fedb840169c6ff0dbe6d642
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-07-20 23:51:34 +0300

    dev cookie support

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,6 @@ export const SessionCookieOptions: CookieOptions = {
     maxAge: 2 * 7 * 24 * 60 * 60,
     sameSite: 'none',
     secure: true,
-    domain: '.radardao.xyz'
+    domain:
+        process.env.NODE_ENV === 'development' ? 'localhost' : '.radardao.xyz'
 };
