ef0ad04c53a23d22b8de5e4a80e2fe891948cb26
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-07-31 15:09:45 +0300

    admin pool management

--- src/paths/pools.ts
+++ src/paths/pools.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import { read, readMany } from '../util/crud';
+import { create, del, read, readMany, update } from '../util/crud';
 import Pool from '../models/Pool';
 import Project from '../models/Project';
+import { authenticate } from '../util/auth';
+import rl from '../ratelimit';
 
 const PoolsRouter = Router();
 
@@ -14,6 +16,26 @@ PoolsRouter.get(
     readMany(Project, () => true, {
         filter: (req) => ({ pool: { $eq: req.params.id } })
     })
+);
+
+PoolsRouter.use(authenticate(true));
+
+PoolsRouter.post(
+    '/',
+    rl('PoolCreate', 60, 5),
+    create(Pool, () => false)
+);
+
+PoolsRouter.patch(
+    '/:id',
+    rl('PoolChange', 60, 10),
+    update(Pool, () => false)
+);
+
+PoolsRouter.delete(
+    '/:id',
+    rl('PoolChange', 60, 5),
+    del(Pool, () => false)
 );
 
 export default PoolsRouter;
