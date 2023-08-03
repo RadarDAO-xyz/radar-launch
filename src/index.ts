772f686de872b69c2a930dbd3f068c0d2fb3633d
marcuspang-cliniserve <marcus.pang@cliniserve.de>
2023-08-03 21:28:56 +0200

    feat: add temp origin

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,8 @@ app.use(
             'https://www.launch.radardao.xyz',
             'https://launch.radardao.xyz',
             'https://radar-launch.netlify.app',
-            'https://radarlaunch.app'
+            'https://radarlaunch.app',
+            'https://radar-launch-frontend.vercel.app'
         ],
         credentials: true
     })
