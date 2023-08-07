71a38eef9e58abe742ca2db391bea6e8bb7cc0d1
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-08-07 13:54:21 +0300

    undo latest changes

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import ProjectsRouter from './paths/projects';
 import UsersRouter from './paths/users';
 import PoolsRouter from './paths/pools';
 import VerifyRouter from './paths/verify';
-import MetadataRouter from './paths/metadata';
 
 const Routes = Router();
 
@@ -11,6 +10,5 @@ Routes.use('/pools', PoolsRouter);
 Routes.use('/projects', ProjectsRouter);
 Routes.use('/users', UsersRouter);
 Routes.use('/verify', VerifyRouter);
-Routes.use('/metadata', MetadataRouter);
 
 export default Routes;
